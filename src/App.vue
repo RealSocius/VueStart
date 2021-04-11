0332981ce856ecba8d983a8d94f34383c471e0c4
Sebastian <mineplayer300guy@gmail.com>
2021-04-11 11:20:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <router-view />
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
